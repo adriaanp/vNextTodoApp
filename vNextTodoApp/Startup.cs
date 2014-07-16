b85939828d24f0b781cf2d98489ae8a6896c9955
Adriaan Putter <adriaan.putter@gmail.com>
2014-07-16 11:22:03 +0200

    Initial commit

---
+++ vNextTodoApp/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Routing;
+
+namespace vNextTodoApp
+{
+    public class Startup
+    {
+        public void Configure(IBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+            app.UseServices(services =>
+            {
+                services.AddMvc();
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "api",
+                    template: "{controller}/{id?}"
+                );
+            });
+        }
+    }
+}
